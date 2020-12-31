711eb05515a26afa20fd1135659e1b2644aed7df
Miklaan <milance_kucevo@hotmail.com>
2020-12-31 16:39:45 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// Mobile Menu Toggle Button JavaScript
+function naviBar() {
+  var x = document.getElementById("m-navibar");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
